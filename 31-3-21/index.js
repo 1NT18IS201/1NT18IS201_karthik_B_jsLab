22a2faf585c130c5c1d31821da6ab1a970c6bcef
KarronAvuche <60656128+KarronAvuche@users.noreply.github.com>
2021-03-31 09:40:04 +0530

    Create index.js

---
+++ 31-3-21/index.js
@@ -0,0 +1,38 @@
+/* console.log("Hello world!");
+
+let a=5,b=5;
+console.log(a);
+let x = (a,b) => {
+  (a+b)
+}
+
+console.log(x); */
+
+function what() {
+  console.log("hello");
+}
+
+// what();
+
+// const sum = function() {
+//   console.log("hey there")
+// }
+
+// sum()
+
+// const hey = (a) => {
+//   console.log(a)
+// }
+
+// hey("hey")
+(function() {
+  console.log("hey there karthicc")
+});
+
+const heyThere = (val) => {
+  const hey = () => console.log("hey fn");
+
+  hey();
+};
+
+heyThere("hsdkb");
