e46a2d0bf009bd7de95d2b238e3637b8132984f4
1NT18IS201 <60656128+1NT18IS201@users.noreply.github.com>
2021-04-30 13:36:01 +0530

    Add files via upload

---
+++ castle.js
@@ -0,0 +1,77 @@
+const b1 = document.getElementById("b1");
+const b2 = document.getElementById("b2");
+const b3 = document.getElementById("b3");
+const b4 = document.getElementById("b4");
+var arr = [];
+var len = 0;
+let flag = false;
+// let flag2 = false;
+b1.addEventListener("mouseenter", () => {
+  console.log("Entered Area 1");
+  arr.push("Entered Area 1");
+  len = arr.length;
+  document.getElementById("output").innerHTML = arr[len - 1];
+});
+
+b2.addEventListener("mouseenter", () => {
+  console.log("Entered Area 2");
+  arr.push("Entered Area 2");
+  len = arr.length;
+  document.getElementById("output").innerHTML = arr[len - 1];
+});
+
+b3.addEventListener("mouseenter", () => {
+  console.log("Entered Area 3");
+  arr.push("Entered Area 3");
+  len = arr.length;
+  document.getElementById("output").innerHTML = arr[len - 1];
+});
+
+b1.addEventListener("mouseleave", () => {
+  console.log("Escaping Area 1");
+  arr.push("Escaping Area 1");
+  len = arr.length;
+  document.getElementById("output").innerHTML = arr[len - 1];
+});
+
+b2.addEventListener("mouseleave", () => {
+  console.log("Escaping Area 2");
+  arr.push("Escaping Area 2");
+  len = arr.length;
+  document.getElementById("output").innerHTML = arr[len - 1];
+});
+
+b3.addEventListener("mouseleave", () => {
+  console.log("Escaping Area 3");
+  arr.push("Escaping Area 3");
+  len = arr.length;
+  document.getElementById("output").innerHTML = arr[len - 1];
+});
+
+b4.addEventListener("mouseenter", () => {
+  console.log("Entered Final area, leave without clicking and die");
+  arr.push("Entered Final area, leave without clicking and die");
+  len = arr.length;
+  document.getElementById("output").innerHTML = arr[len - 1];
+});
+
+b4.addEventListener("click", () => {
+  flag = true;
+});
+
+b4.addEventListener("mouseleave", () => {
+  if (!flag) {
+    console.log("dead");
+    arr.push("Dead");
+    len = arr.length;
+    document.getElementById("output").innerHTML = arr[len - 1];
+  } else {
+    console.log("alive");
+    arr.push("Alive");
+    len = arr.length;
+    document.getElementById("output").innerHTML = arr[len - 1];
+    flag = false;
+  }
+});
+
+// document.getElementById("output").innerHTML = "abc";
